8519bfbeacf83e25b07e0087e2b3a5b70df9e469
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-20 18:53:33 +0530

    [Kavyasree J Ganesh] largestamong3numbers

---
+++ src/com/bridgelabz/Largest_Among_3Numbers.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class Largest_Among_3Numbers {
+    static void largest(int num1, int num2, int num3){
+        if(num1 > num2){
+            if(num1 > num3){
+                System.out.println( num1 + " is Largest ");
+            }else{
+                System.out.println( num2 + " is Largest ");
+            }
+
+        }else{
+            if(num3 > num2){
+                System.out.println( num3 + " is Largest ");
+            }else{
+                System.out.println( num2 + " is Largest ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        largest(45, 32, 17);
+    }}
