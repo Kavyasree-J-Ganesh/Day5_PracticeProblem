aee767e53a1b15e211f64106d6447d696be3e06e
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-20 19:29:08 +0530

    [Kavyasree J Ganesh] leapyear

---
+++ src/com/bridgelabz/LeapYear.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class LeapYear {
+    static void checkLeapYear(int year) {
+
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println( year + " is Leap Year ");
+        }else {
+            System.out.println( year + " is Not a Leap Year ");
+        }
+}
+    public static void main(String[] args) {
+        checkLeapYear(2000);
+    }
+}
