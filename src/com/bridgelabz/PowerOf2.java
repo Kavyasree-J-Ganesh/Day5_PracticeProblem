05a69b9edcab4e5f075d9f7e7ccda85d4ea68a93
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-20 23:16:07 +0530

    [Kavyasree J Ganesh] power

---
+++ src/com/bridgelabz/PowerOf2.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PowerOf2 {
+    public static void main(String[] args) {
+        int N = Integer.valueOf(args[0]);
+        int mul = 1;
+        for(int i=0; i<=N; i++) {
+            mul = mul * 2;
+            System.out.println(mul);
+        }
+    }
+}
+
