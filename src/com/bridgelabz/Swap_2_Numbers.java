aecd75553c627e7c6868458325785d875eb50c70
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-21 00:12:42 +0530

    [Kavyasree J Ganesh] swap_numbers

---
+++ src/com/bridgelabz/Swap_2_Numbers.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Swap_2_Numbers {
+    static void swapNumbers(int a, int b){
+
+        System.out.println(" Value of a is " + a);
+        System.out.println(" Value of b is " + b);
+
+       int swap = a;
+       a = b;
+       b = swap;
+
+        System.out.println(" Value of a after swap is " + a);
+        System.out.println(" Value of b after swap is " + b);
+    }
+
+    public static void main(String[] args) {
+        swapNumbers(2,5);
+    }
+}
